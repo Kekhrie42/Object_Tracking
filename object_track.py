a5382559d05396db8dbc207202857ee76af99226
Kekhrie42 <116770407+Kekhrie42@users.noreply.github.com>
2023-01-23 16:21:33 -0700

    Update object_track.py

--- object_track.py
+++ object_track.py
@@ -5,8 +5,8 @@ def main():
     df = pd.read_table("test.tsv")
 
     #Adds id column into the data frame
-    df['ID'] = "None"
-    df = add_id(df)
+    df['ID'] = "None" #Initializing all ID's to None
+    df = add_id_to_grain(df)
     
     df.to_csv('example.tsv', sep="\t")
 
@@ -15,10 +15,10 @@ def main():
 Function that adds id into the data frame.
 df: Original Data frame
 '''
-def add_id(df):
+def add_id_to_grain(df):
     id = 0
     for index, row in df.iterrows():
-        if row['score'] > 0.35:
+        if row['score'] > 0.35 and row['class'] != 'tube_tip':
             df.at[index, 'ID'] = id
             id += 1
     return df
