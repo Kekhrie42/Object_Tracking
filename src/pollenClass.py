187463830645daf215d8b87515a538be1473ebfc
Kekhrie42 <116770407+Kekhrie42@users.noreply.github.com>
2023-05-01 14:18:53 -0700

    Update pollenClass.py

--- src/pollenClass.py
+++ src/pollenClass.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np 
 
 class pollenGrain:
-    def __init__(self):
+    def __init__(self, pid):
         self.position = {}
         self.class_type = {}
         self.time_point = 0
+        self.pid = pid
 
     
     def add_position(self, xCentroid, yCentroid, timePoint):
@@ -36,14 +37,46 @@ class pollenGrain:
     def change_time(self, timePoint):
         self.time_point = timePoint
     
-    # def get_class(self):
-    #     return self.class_type[self.time_point]
-
-    # def populate_position(self, xCentroid, yCentroid, timePoint):
-    #     self.position[timePoint] = (xCentroid, yCentroid)
+    def get_id(self):
+        return self.pid
     
-    # def populate_class(self, pollenClass, timePoint):
-    #     self.class_type[timePoint] = pollenClass
+
+class tubeTip:
+    def __init__(self, pid):
+        self.position = {}
+        self.time_point = 0
+        self.pid = pid
+        self.class_type = {}
+
+    def add_position(self, xCentroid, yCentroid, timePoint):
+        self.position[timePoint] = (xCentroid, yCentroid)
+        self.time_point = timePoint
+
+    def update_position(self, timePoint, coords):
+        self.position[timePoint] = coords
+        self.time_point = timePoint
+
+    def get_position(self):
+        return self.position
+    
+    def add_class(self, pollenClass, timePoint):
+        self.class_type[timePoint] = pollenClass
+        self.time_point = timePoint
+
+    def get_class(self):
+        return self.class_type
+    
+    def get_xcentroid(self):
+        return (self.position[self.time_point])[0]
+    
+    def get_ycentroid(self):
+         return (self.position[self.time_point])[1]
+
+    def change_time(self, timePoint):
+        self.time_point = timePoint
+    
+    def get_id(self):
+        return self.pid
 
     
     
